22bdba064bb725d5efa8342b809e67b5a72cd66c
Fellipe Soares <soaresfellipe@outlook.com>
2014-06-18 20:58:42 -0300

    [UPD] Inclusao Junit

---
+++ webapp/ubs/src/test/java/br/com/ubs/jdbc/dao/UBSDaoTest.java
@@ -0,0 +1,20 @@
+package br.com.ubs.jdbc.dao;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import br.com.ubs.cadastro.UBS;
+
+public class UBSDaoTest {
+	
+	@Test
+	public void inclusaoUBS() {
+		
+		Calendar hoje = Calendar.getInstance();
+		
+		UBS ubs1 = new UBS();
+		
+	}
+
+}
