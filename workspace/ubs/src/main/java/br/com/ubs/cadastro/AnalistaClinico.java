49f0c19dd2c16b59ca528499b0576df9e1048223
Fellipe Soares <soaresfellipe@outlook.com>
2014-06-17 23:22:09 -0300

    Primeiro commmit do webapp

---
+++ workspace/ubs/src/main/java/br/com/ubs/cadastro/AnalistaClinico.java
@@ -0,0 +1,15 @@
+package br.com.ubs.cadastro;
+
+public class AnalistaClinico extends Funcionario {
+	
+	public int crf;
+
+	public int getCrf() {
+		return crf;
+	}
+
+	public void setCrf(int crf) {
+		this.crf = crf;
+	}
+
+}
---
+++ workspace/ubs/src/main/java/br/com/ubs/cadastro/Medico.java
@@ -0,0 +1,15 @@
+package br.com.ubs.cadastro;
+
+public class Medico extends Funcionario {
+	
+	public int crm;
+
+	public int getCrm() {
+		return crm;
+	}
+
+	public void setCrm(int crm) {
+		this.crm = crm;
+	}
+
+}
